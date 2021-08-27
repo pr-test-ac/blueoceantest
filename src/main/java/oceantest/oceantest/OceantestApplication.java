674475ab65499c9ce08c5de503fc01a891891f23
Pawan <pr.test.ac@gmail.com>
2021-08-27 12:25:31 -0400

    first commit

---
+++ src/main/java/oceantest/oceantest/OceantestApplication.java
@@ -0,0 +1,13 @@
+package oceantest.oceantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceantestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceantestApplication.class, args);
+	}
+
+}
